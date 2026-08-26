593dd8310afd0b21deb63ae2bf754a530ed82da6
Gemma Krebs <96151596+ge-krebs@users.noreply.github.com>
2026-08-26 19:19:41 +0700

    landing page + json text

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,8 +1,37 @@
+import text from '../data/text.json'
+// import { useState, useEffect } from 'react'
+
+// function typewriter (text, speed) {
+//     useEffect(() => {
+
+//     })
+// }
+
 function Landing () {
 
     return (
         <>
-        <h1 className="bg-pale-blue">test</h1>
+        <div className="flex flex-col md:flex-row p-5 md:p-5">
+            {/* col left */}
+            <div className="md:w-1/2 items-center">
+                <img className="w-full" src="memoji-laptop.png" />
+            </div>
+            {/* col right */}
+            <div className="md:w-1/2 justify-end pt-5">
+                <div className="flex flex-col justify-end h-full">
+                    <h1 className="text-white text-7xl">
+                        {text.landing.title}
+                    </h1>
+                    <p className="text-sm pt-4">
+                        <span className="text-pale-blue text-3xl">{text.landing.name}</span>
+                        {text.landing.body}
+                    </p>
+                    <p className="pt-2 text-sm">
+                        {text.landing.subBody}
+                    </p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
